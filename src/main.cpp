48730b1dc4e8ea77218761c19f795c4d2baa5602
Nathan Smythe <nmsmythe24@gmail.com>
2026-02-08 15:19:06 -0500

    test again

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 #include "FastAccelStepper.h"
+#include <Servo.h>
 
-#define dirPinStepper    14
 #define enablePinStepper 23
-#define stepPinStepper   27
 
-FastAccelStepperEngine engine;
-FastAccelStepper* stepper = nullptr;
+#define dirPinStepper1    14
+#define stepPinStepper1   27
+
+#define dirPinStepper2    13
+#define stepPinStepper2   26
+
+
+#define servoPin1 25
+#define servoPin2 12
+
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepper1 = NULL;
+FastAccelStepper *stepper2 = NULL;
+Servo servo1;
+Servo servo2;
 
 void setup() {
   Serial.begin(115200);
@@ -14,41 +26,112 @@ void setup() {
   Serial.println();
   Serial.println("=== FastAccelStepper ESP32 test ===");
 
-  /**
-   * "s,c,x,y"
-   * s:0,1,2
-   * c:[0,5]
-   * x,y: [range]
-   */
-
   engine.init();
   Serial.println("Engine init done");
 
-  stepper = engine.stepperConnectToPin(stepPinStepper);
-  if (!stepper) {
+  stepper1 = engine.stepperConnectToPin(stepPinStepper1);
+  stepper2 = engine.stepperConnectToPin(stepPinStepper2);
+
+  if (!stepper1 || !stepper2) {
     Serial.println("ERROR: stepper is NULL! Check stepPinStepper and wiring.");
     while (true) {
       delay(1000);  // halt so we don't crash dereferencing a null pointer
     }
   }
-  
   Serial.println("Stepper connected OK");
 
-  stepper->setDirectionPin(dirPinStepper);
-  stepper->setEnablePin(enablePinStepper);
-  stepper->setAutoEnable(true);
+  stepper1->setDirectionPin(dirPinStepper1);
+  stepper1->setEnablePin(enablePinStepper);
+  stepper1->setAutoEnable(true);
 
-  stepper->setSpeedInHz(1000);    // try a bit faster to clearly see motion
-  stepper->setAcceleration(500);
+  stepper2->setDirectionPin(dirPinStepper2);
+  stepper2->setEnablePin(enablePinStepper);
+  stepper2->setAutoEnable(true);
+
+  stepper1->setSpeedInHz(1000);    // try a bit faster to clearly see motion
+  stepper1->setAcceleration(800);
+
+  stepper2->setSpeedInHz(500);    // try a bit faster to clearly see motion
+  stepper2->setAcceleration(800);
 
   Serial.println("Stepper configured");
+
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+
+  Serial.println("Servos configured");
+
 }
 
 void loop() {
-  Serial.println("HI");
-  if (stepper && !stepper->isRunning()) {
-    Serial.println(stepper->getCurrentPosition());
-    stepper->move(2000);
+  if (stepper1 && !(stepper1->isRunning())) {
+    stepper1->runForward();
+    // Serial.println(stepper1->getCurrentPosition());
+    // stepper1->move(2000);
   }
-  delay(10);
+  if (stepper2 && !(stepper2->isRunning())) {
+  stepper2->runBackward();
+
+  // Serial.println(stepper1->getCurrentPosition());
+  // stepper1->move(2000);
+  }
+  if(servo1.read() == 180){
+    servo1.write(0);
+  } else {
+    servo1.write(180);
+  }
+  if(servo2.read() == 180){
+    servo2.write(0);
+  } else {
+    servo2.write(180);
+  }
+  delay(1000);
 }
+
+
+
+
+
+
+// void setup() {
+//   engine.init();
+//   stepper1 = engine.stepperConnectToPin(stepPinStepper1);
+//   Serial.begin(115200);
+//   if (stepper1) {
+//     stepper1->setDirectionPin(dirPinStepper1);
+//     stepper1->setAutoEnable(true);
+
+//     stepper1->setSpeedInHz(500);       // 500 steps/s
+//     stepper1->setAcceleration(100);    // 100 steps/s²
+//     stepper1->move(1000);
+//   }
+// }
+
+// void loop() {
+//   //Serial.println("Hello");
+//   stepper1->runForward();
+// }
+
+// // put function declarations here:
+// int myFunction(int, int);
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
+
+
+// FastAccelStepperEngine engine;
+// FastAccelStepper* stepper = nullptr;
+
+
